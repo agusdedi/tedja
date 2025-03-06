ac123223f695940574643ac0e0fcd828f1bf29d0
Agus Dedi <dedipriyana16@gmail.com>
2025-03-06 11:19:43 +0800

    Init project. Migration and model database

---
+++ app/Models/HouseFacility.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class HouseFacility extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $fillable = ['house_id', 'facility_id'];
+
+    public function houses()
+    {
+        return $this->belongsTo(House::class, 'house_id');
+    }
+
+    public function facilities()
+    {
+        return $this->belongsTo(Facility::class, 'facility_id');
+    }
+}
